6144a0bbceb3b9163d69295bf3650037e09e1e0d
Arnab <arnabchatterjee.ac.2@gmail.com>
2022-05-31 22:45:34 +0530

    added deleter

--- @@OOP/property decorator/Employee.py
+++ @@OOP/property decorator/Employee.py
@@ -5,6 +5,9 @@
     But we cant set value to it means if we do emp1.fullname = "arnab ch" then we
     will be getting error can't set attribute. 
     For this we need setter
+
+    TODO: deleter
+    If we want to do a clean up code when we are deleting the attributes we can use it
 """
 class Emp:
     def __init__(self,first,last ):
@@ -24,7 +27,20 @@ class Emp:
         self.first = first
         self.last = last
 
+    @fullname.deleter
+    def fullname(self):
+        print("Delete name")
+        self.first = None
+        self.last = None
+
 emp1 = Emp("arnab","ch")
+# using property
 print(emp1.email) # email is method but using property we can use it as an attribute
+
+# using setter
 print(emp1.fullname)
 emp1.fullname = "arnab ch"
+
+#using deleter
+del emp1.fullname
+print(emp1.fullname)
